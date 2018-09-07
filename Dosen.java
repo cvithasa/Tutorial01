43a640b8abca6d22f061fd5c1daf1beb4f937749
cvithasa <cvithasaik@gmail.com>
2018-09-07 16:50:28 +1300

    Commit file Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,19 @@
+public class Dosen{
+	private String nama;
+	private String ajarMataKuliah;
+	
+	public Dosen(String nama, String ajar) {
+		this.nama = nama;
+		this.ajar = ajarMataKuliah;
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	
+	public String getMataKuliah() {
+		return ajarMataKuliah;
+	}
+	
+	
+}
