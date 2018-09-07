0c81ceff59a65e9a6512a18e21f15b61a7f72660
cvithasa <cvithasaik@gmail.com>
2018-09-07 16:41:42 +1300

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,19 @@
+public class Mahasiswa{
+	private String nama;
+	private long npm;
+	
+	public Mahasiswa(String nama, long npm) {
+		this.nama = nama;
+		this.npm = npm;
+	}
+	
+	public String getNama() {
+		return nama;
+	}
+	
+	public long getNPM() {
+		return npm;
+	}
+	
+	
+}
